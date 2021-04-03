3b4a783853c8a329bd29729004b569078d6be3e3
Dlrmhcr <63908479+Dlrmhcr@users.noreply.github.com>
2021-04-03 08:45:22 +0300

    adding new file

---
+++ find the biggest one.py
@@ -0,0 +1,11 @@
+print(" üç farkli sayi giriniz ")
+a = input( " birinci sayi ")
+b = input( " ikinci sayi ")
+c = input( " üçüncü sayı ")
+if ( a > b and a > c ):
+    print(" en büyük sayi :" + str(a) + " dir")
+elif ( b > a and b > c ):
+    print(" en büyük sayi :" + str(b) + " dir")
+else :
+     print(" en büyük sayi :" + str(c) + " dir")
+    
